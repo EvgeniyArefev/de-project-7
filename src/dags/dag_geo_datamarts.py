2fc5b0f1b7af82974037d1004b4fbf456298cdc2
Evgeniy Arefev <evgeniyarefev@gmail.com>
2023-05-24 09:09:22 +0300

    refactor dag code

--- src/dags/dag_geo_datamarts.py
+++ src/dags/dag_geo_datamarts.py
@@ -10,6 +10,10 @@ os.environ['JAVA_HOME']='/usr'
 os.environ['SPARK_HOME'] ='/usr/lib/spark'
 os.environ['PYTHONPATH'] ='/usr/local/lib/python3.8'
 
+path_events = "/user/johnafv/project_7/events_sample"
+path_geo_cities = "/user/johnafv/project_7/geo"
+path_to_write = "/user/johnafv/project_7/marts"
+
 default_args = {
     'owner': 'airflow',
     'start_date':datetime(2020, 1, 1)
@@ -27,9 +31,9 @@ dm_users = SparkSubmitOperator(
     application = '/lessons/datamart_users.py',
     conn_id= 'yarn_spark',
     application_args = [ 
-        "/user/johnafv/project_7/events_sample", 
-        "/user/johnafv/project_7/geo", 
-        "/user/johnafv/project_7/marts/mart_users"
+        f"{path_events}", 
+        f"{path_geo_cities}", 
+        f"{path_to_write}/mart_users"
         ],
     conf={
         "spark.driver.maxResultSize": "20g"
@@ -38,38 +42,38 @@ dm_users = SparkSubmitOperator(
     executor_memory = '1g'
 )
 
-# dm_zones = SparkSubmitOperator(
-#     task_id = 'dm_zones',
-#     dag = dag_spark,
-#     application = '/lessons/datamart_zones.py',
-#     conn_id= 'yarn_spark',
-#     application_args = [ 
-#         "/user/johnafv/data/geo/events_light/", 
-#         "/user/johnafv/data/geo/geo.csv", 
-#         "/user/johnafv/prod/datamart_zones/"
-#         ],
-#     conf={
-#         "spark.driver.maxResultSize": "20g"
-#     },
-#     executor_cores = 1,
-#     executor_memory = '1g'
-# )
+dm_zones = SparkSubmitOperator(
+    task_id = 'dm_zones',
+    dag = dag_spark,
+    application = '/lessons/datamart_zones.py',
+    conn_id= 'yarn_spark',
+    application_args = [ 
+        f"{path_events}", 
+        f"{path_geo_cities}",  
+        f"{path_to_write}/mart_zones"
+        ],
+    conf={
+        "spark.driver.maxResultSize": "20g"
+    },
+    executor_cores = 2,
+    executor_memory = '1g'
+)
 
-# dm_friends_recomendation = SparkSubmitOperator(
-#     task_id = 'dm_friends_recomendation',
-#     dag = dag_spark,
-#     application = '/lessons/datamart_friends_recomendation.py',
-#     conn_id= 'yarn_spark',
-#     application_args = [ 
-#         "/user/johnafv/data/geo/events_light/", 
-#         "/user/johnafv/data/geo/geo.csv", 
-#         "/user/johnafv/prod/datamart_friends_recomendation/"
-#         ],
-#     conf={
-#         "spark.driver.maxResultSize": "20g"
-#     },
-#     executor_cores = 1,
-#     executor_memory = '1g'
-# )
+dm_friends_recomendation = SparkSubmitOperator(
+    task_id = 'dm_friends_recomendation',
+    dag = dag_spark,
+    application = '/lessons/datamart_friends_recomendation.py',
+    conn_id= 'yarn_spark',
+    application_args = [ 
+        f"{path_events}", 
+        f"{path_geo_cities}",  
+        f"{path_to_write}/mart_friends_recomendation"
+        ],
+    conf={
+        "spark.driver.maxResultSize": "20g"
+    },
+    executor_cores = 1,
+    executor_memory = '1g'
+)
 
 
